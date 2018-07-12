74f5dbdac11647e9a398fa880f65563269e4246f
Joan Perals <joan.perals@ixds.com>
2018-07-13 01:13:07 +0200

    Use Markdown

--- til/settings.py
+++ til/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'learned.apps.LearnedConfig',
+    'django_markdown2',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

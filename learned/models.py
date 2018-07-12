23cb684e683c831ac6b9146a78ec0a92e5b738a9
Joan Perals <joan.perals@ixds.com>
2018-07-12 23:48:08 +0200

    New Learning model

--- learned/models.py
+++ learned/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
-# Create your models here.
+class Learning(models.Model):
+    learning_text = models.TextField(max_length=3000)
--- til/settings.py
+++ til/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'learned.apps.LearnedConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

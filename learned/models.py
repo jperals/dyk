dd51bc59a11b5a66e211f824cc7f155c00f33a8d
Joan Perals <joan.perals@ixds.com>
2018-07-12 23:58:55 +0200

    Show ellipsised content in learning list in admin interface

--- learned/models.py
+++ learned/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
+maxShortTitleLength = 50
+
 class Learning(models.Model):
     learning_text = models.TextField(max_length=3000)
+
+    def __str__(self):
+        length = len(self.learning_text)
+        if length > maxShortTitleLength + 1:
+            title = self.learning_text[:maxShortTitleLength] + '…'
+        else:
+            title = self.learning_text
+        return title

5171c7cb6e73bfaa895f7583f47d68ee7bfbad8e
Joan Perals <joan.perals@ixds.com>
2018-07-20 23:06:33 +0200

    Add new title field to Learning model

--- learned/models.py
+++ learned/models.py
@@ -2,13 +2,23 @@ from django.db import models
 
 maxShortTitleLength = 50
 
+
 class Learning(models.Model):
+    learning_title = models.CharField(
+        blank=True,
+        max_length=3000
+    )
     learning_text = models.TextField(max_length=3000)
 
     def __str__(self):
-        length = len(self.learning_text)
-        if length > maxShortTitleLength + 1:
-            title = self.learning_text[:maxShortTitleLength] + '…'
-        else:
+        # If the learning has a title, use it as excerpt in lists.
+        # Otherwise, use a fragment from the content instead.
+        # In any case, shorten to a maximum length.
+        title = self.learning_title
+        length = len(title)
+        if length is 0:
             title = self.learning_text
+            length = len(title)
+        if length > maxShortTitleLength + 1:
+            title = title[:maxShortTitleLength] + '…'
         return title

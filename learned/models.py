4cd94b9c87455fede337a2d513643219fefa00a9
Joan Perals <joan.perals@ixds.com>
2018-07-28 15:28:37 +0200

    Add created_date and modified_date to Learning model

--- learned/models.py
+++ learned/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.utils import timezone
 
 maxShortTitleLength = 50
 
@@ -9,6 +11,9 @@ class Learning(models.Model):
         max_length=3000
     )
     learning_text = models.TextField(max_length=3000)
+
+    created_date = models.DateTimeField(blank=True)
+    modified_date = models.DateTimeField(editable=False)
 
     def __str__(self):
         # If the learning has a title, use it as excerpt in lists.
@@ -22,3 +27,19 @@ class Learning(models.Model):
         if length > maxShortTitleLength + 1:
             title = title[:maxShortTitleLength] + '…'
         return title
+
+
+# The fields created_date and modified_date are automatically created/modified but are still editable.
+# Approach mostly based on:
+# https://stackoverflow.com/questions/1737017/django-auto-now-and-auto-now-add#1737078
+# But using pre_save signal instead of overriding the save method, like here:
+# https://www.codingforentrepreneurs.com/blog/post-save-vs-pre-save-vs-override-save-method/
+
+def add_dates(sender, instance, *args, **kwargs):
+    # On save, update timestamps
+    if not instance.id:
+        instance.created_date = timezone.now()
+    instance.modified_date = timezone.now()
+
+
+pre_save.connect(add_dates, sender=Learning)

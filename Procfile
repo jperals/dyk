3bebb08b9bbb449065c46eedc0f405c190db85c4
Joan Perals <joan.perals@ixds.com>
2018-08-31 20:07:54 +0200

    Rename app to "dyk"

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate --noinput
-web: gunicorn til.wsgi
+web: gunicorn dyk.wsgi
--- dyk/wsgi.py
+++ dyk/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for til project.
+WSGI config for dyk project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "til.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dyk.settings")
 
 application = get_wsgi_application()

6f80830a077c6c003978877a52d88f017648ea8c
Joan Perals <jperals@protonmail.com>
2019-08-16 00:41:32 +0200

    Add JSON REST API

--- dyk/settings.py
+++ dyk/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
--- learned/urls.py
+++ learned/urls.py
@@ -1,8 +1,34 @@
+from django.conf.urls import url, include
 from django.urls import path
+from rest_framework import routers, serializers, viewsets
+from .models import Learning, Tag
 
 from . import views
 
+# Serializers define the API representation.
+class TagSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+    
+class LearningSerializer(serializers.HyperlinkedModelSerializer):
+    tags = TagSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Learning
+        fields = ['id', 'learning_title', 'learning_text', 'created_date', 'modified_date', 'tags', '__str__']
+    
+# ViewSets define the view behavior.
+class LearningViewSet(viewsets.ModelViewSet):
+    queryset = Learning.objects.all()
+    serializer_class = LearningSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'learnings', LearningViewSet)
+
 urlpatterns = [
+    url(r'^api/', include(router.urls)),
     path('', views.index, name='index'),
     path('learning/<int:id>', views.learning, name='learning'),
 ]
